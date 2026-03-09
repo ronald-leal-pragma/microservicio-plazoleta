ade7c2620af4e68f0957dcb13cccdb2cdb7b07d7
ronald.leal_pragma <ronald.leal@pragma.com.co>
2026-03-09 09:01:15 -0500

    feat: Se agrega clase para capturar excepciones

--- src/main/java/com/pragma/plazoleta/infrastructure/exceptionhandler/ExceptionResponse.java
+++ src/main/java/com/pragma/plazoleta/infrastructure/exceptionhandler/ExceptionResponse.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum ExceptionResponse {
-    NO_DATA_FOUND("No data found for the requested petition"),
-    USER_ALREADY_EXISTS("A user with the given email or document already exists");
+    NO_DATA_FOUND("No se encontraron datos para la petición solicitada"),
+    USER_ALREADY_EXISTS("Ya existe un usuario con el correo electrónico o documento indicado");
 
     private final String message;
 
